9da582160734bf01efae0e4ea5be54c43a167ff0
N. Shimizu <chikoski@gmail.com>
2017-05-24 14:35:16 +0900

    added an excersise for ownership

---
+++ exercises/ownership/src/main.rs
@@ -0,0 +1,40 @@
+#![allow(dead_code)]
+fn main() {
+    let (word_a, word_b) = words();
+    let message = format!("{}{}！", word_a, word_b);
+    output(message);
+    //output(message);
+}
+
+fn words() -> (String, String) {
+    (format!("こんにちは"), format!("世界"))
+}
+
+fn output(text: String) {
+    let kanji_only = remove_hiragana(text);
+    println!("{}", kanji_only);
+    /*
+    ゴール2：次の行をアンコメントすると何がおきるでしょうか？
+    これをコンパイルを通すにはどうすれば良いでしょうか？
+    */
+    // println("ひらがなを抜き取ると：{:?} → {:?}", text, kanji_only);
+
+    /*
+    ゴール3：データをコピーせずにコンパイルを通すにはどおすれば良いでしょうか？
+    所有権の移動のみを使って解決してください
+    */
+}
+
+fn remove_hiragana(text: String) -> String {
+    /*
+     ゴール1：コンパイルを通すには何を変更すれば良いでしょうか
+    */
+    let result = String::new();
+    for c in text.chars() {
+        if c < 'あ' || 'ん' < c {
+            result.push(c);
+        };
+    }
+    result
+}
+
